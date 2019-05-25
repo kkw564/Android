164f5201e200f0b8b57562dff1df6e8df1132e7d
kkw <kkw564@naver.com>
2019-05-25 15:43:05 +0900

    Make calc program
    - No exception handling, 추후 제작 예정

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addClick(View v){
+        EditText num1 = (EditText) findViewById(R.id.num1_value);
+        EditText num2 = (EditText) findViewById(R.id.num2_value);
+        TextView result = (TextView) findViewById(R.id.resultView);
+
+        int n1 = Integer.parseInt(num1.getText().toString());
+        int n2 = Integer.parseInt(num2.getText().toString());
+        int ret = n1 + n2;
+
+        String str = Integer.toString(ret);
+        result.setText(str);
+        Toast.makeText(MainActivity.this,str, Toast.LENGTH_SHORT).show();
+    }
+    public void subClick(View v){
+        EditText num1 = (EditText) findViewById(R.id.num1_value);
+        EditText num2 = (EditText) findViewById(R.id.num2_value);
+        TextView result = (TextView) findViewById(R.id.resultView);
+
+        int n1 = Integer.parseInt(num1.getText().toString());
+        int n2 = Integer.parseInt(num2.getText().toString());
+        int ret = n1 - n2;
+
+        String str = Integer.toString(ret);
+        result.setText(str);
+        Toast.makeText(MainActivity.this,str, Toast.LENGTH_SHORT).show();
+    }
+
+    public void mulClick(View v){
+        EditText num1 = (EditText) findViewById(R.id.num1_value);
+        EditText num2 = (EditText) findViewById(R.id.num2_value);
+        TextView result = (TextView) findViewById(R.id.resultView);
+
+        int n1 = Integer.parseInt(num1.getText().toString());
+        int n2 = Integer.parseInt(num2.getText().toString());
+        int ret = n1 * n2;
+
+        String str = Integer.toString(ret);
+        result.setText(str);
+        Toast.makeText(MainActivity.this,str, Toast.LENGTH_SHORT).show();
+    }
+
+    public void divClick(View v){
+        EditText num1 = (EditText) findViewById(R.id.num1_value);
+        EditText num2 = (EditText) findViewById(R.id.num2_value);
+        TextView result = (TextView) findViewById(R.id.resultView);
+
+        int n1 = Integer.parseInt(num1.getText().toString());
+        int n2 = Integer.parseInt(num2.getText().toString());
+        int ret = n1 / n2;
+        String str = Integer.toString(ret);
+        result.setText(str);
+        Toast.makeText(MainActivity.this,str, Toast.LENGTH_SHORT).show();
     }
 }
